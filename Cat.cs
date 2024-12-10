ffa1e31d6d706afe48f7ce7a11c2785147b3fb1b
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-12-10 08:57:51 +0100

    Add project files.

---
+++ Cat.cs
@@ -0,0 +1,11 @@
+﻿namespace CA241210;
+
+internal class Cat
+{
+    public Guid Guid { get; set; }
+    public string Species { get; set; }
+    public string Name { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string? Pattern { get; set; }
+    public string? Color { get; set; }
+}
