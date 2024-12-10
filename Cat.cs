40fb934de6fd0a1a0e7233f93be95196774da35e
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-12-10 09:05:32 +0100

    add sex + ts override

--- Cat.cs
+++ Cat.cs
@@ -8,14 +8,20 @@ internal class Cat
     public DateTime BirthDate { get; set; }
     public string? Pattern { get; set; }
     public string? Color { get; set; }
+    public bool Sex { get; set; }
 
-    public Cat(Guid guid, string species, string name, DateTime birthDate, string? pattern, string? color)
+    public override string ToString() => 
+        $"{Name} is a {Color} {Species} cat, {(Sex? "he" : "she")} " +
+        $"is { DateTime.Now.Year - BirthDate.Year} years old.";
+
+    public Cat(string species, string name, string birthDate, string? pattern, string? color, bool sex)
     {
-        Guid = guid;
+        Guid = Guid.NewGuid();
         Species = species ?? throw new ArgumentNullException(nameof(species));
         Name = name ?? throw new ArgumentNullException(nameof(name));
-        BirthDate = birthDate;
+        BirthDate = DateTime.Parse(birthDate);
         Pattern = pattern;
         Color = color;
+        Sex = sex;
     }
 }
