d8265ea84c4f3d7feb064e8a954df315084c8987
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-12-10 09:25:09 +0100

    add alt ctor

--- Cat.cs
+++ Cat.cs
@@ -10,14 +10,31 @@ internal class Cat
     public string? Color { get; set; }
     public bool Sex { get; set; }
 
-    public override string ToString() => 
-        $"{Name} is a {Color} {Species} cat, {(Sex ? "he" : "she")} " +
-        $"is { DateTime.Now.Year - BirthDate.Year} years old.";
+    public override string ToString()
+    {
+        string patColStr = Color is null || Pattern is null ?
+            "unknown" : $"{Pattern} {Color}";
 
-    public Cat(string species, string name, string birthDate, string? pattern, string? color, bool sex)
+        return $"{Name} is a {patColStr} {Species} cat, {(Sex ? "he" : "she")} " +
+            $"is {DateTime.Now.Year - BirthDate.Year} years old.";
+    }
+
+
+    public Cat(string name, string birthDate, string? pattern, string? color, bool sex, string species)
     {
         Guid = Guid.NewGuid();
         Species = species ?? throw new ArgumentNullException(nameof(species));
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        BirthDate = DateTime.Parse(birthDate);
+        Pattern = pattern;
+        Color = color;
+        Sex = sex;
+    }
+
+    public Cat(string name, string birthDate, bool sex, string? pattern = null, string? color = null)
+    {
+        Guid = Guid.NewGuid();
+        Species = "domestic";
         Name = name ?? throw new ArgumentNullException(nameof(name));
         BirthDate = DateTime.Parse(birthDate);
         Pattern = pattern;
--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using CA241210;
 
-Cat cat = new(
-    species: "domestic",
-    name: "Puc-Puc",
-    birthDate: "2021-06-30",
-    pattern: "tabby",
-    color: "gray",
-    true);
+//Cat cat = new(
+//    species: "persian",
+//    name: "Puc-Puc",
+//    birthDate: "2021-06-30",
+//    pattern: "tabby",
+//    color: "gray",
+//    sex: true);
+
+
 
 Console.WriteLine(cat);
